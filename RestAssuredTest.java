fcee093820cc87bacf8985471098dafd3030de82
kiranyadav245 <kiranyadav245@gmail.com>
2017-11-17 18:04:19 +0530

    Update and rename RestTEstGet.java to RestAssuredTest.java

--- RestAssuredTest.java
+++ RestAssuredTest.java
@@ -11,7 +11,7 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
  
-public class RestTEstGet {
+public class RestAssuredTest {
 	
 	@Test
 	public void GetWeatherDetails()
